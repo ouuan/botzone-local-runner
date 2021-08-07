97a56d581d33366b4b6d5d98ef28de1cad23df31
Yufan You <ouuansteve@gmail.com>
2021-08-07 11:35:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Botzone local runner
+
+Run Botzone judger & bots at local.
+
+You need:
+
+-   Several [bots](https://wiki.botzone.org.cn/index.php?title=Bot).
+-   A [judger](https://wiki.botzone.org.cn/index.php?title=%E8%A3%81%E5%88%A4).
+-   Run `yarn`.
+-   Run `yarn start -b path/to/bot1 path/to/bot2 ... -j path/to/judger`.
+
+The bots and the judger must be executable. You can use a bash script if the code of your bot/judger is not self-executable.
+
+The number of bots is the number of players. The same bot file can be repeated.
+
+The outputs contain the `display` attribute from the judger.
---
+++ runner.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright [yyyy] [name of copyright owner]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+const { execSync } = require('child_process');
+const { program } = require('commander');
+const { version } = require('./package.json');
+
+program.version(version)
+  .requiredOption('-j, --judger <judger>', 'Executable judger program')
+  .requiredOption('-b, --bot <bots...>', 'Executable bot programs')
+  .parse();
+
+const { judger, bot } = program.opts();
+
+const log = [];
+const requests = Array.from(Array(bot.length), () => []);
+const responses = Array.from(Array(bot.length), () => []);
+const data = Array(bot.length);
+const globaldata = Array(bot.length);
+let initdata = "";
+
+for (let round = 0; ; ++round) {
+  const output = JSON.parse(execSync(judger, { input: JSON.stringify({ log, initdata }) }));
+  const { command, content, display } = output;
+  if (round === 0 && output.initdata !== void (0)) initdata = output.initdata;
+  console.log(display);
+  log.push({ output });
+  if (command === 'finish') {
+    console.log('----- FINISHED -----');
+    console.log(content);
+    break;
+  } else if (command !== 'request') {
+    console.error(`Invalid command: ${command}`);
+    break;
+  }
+
+  const res = {};
+  for (const [player, request] of Object.entries(content)) {
+    requests[player].push(request);
+    const input = JSON.stringify({
+      requests: requests[player],
+      responses: responses[player],
+      data: data[player],
+      globaldata: globaldata[player]
+    });
+    const botOutput = JSON.parse(execSync(bot[player], { input }));
+    data[player] = botOutput.data;
+    globaldata[player] = botOutput.globaldata;
+    res[player] = { response: botOutput.response };
+    responses[player].push(botOutput.response);
+  }
+  log.push(res);
+}
