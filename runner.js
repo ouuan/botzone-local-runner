1eb140ba6544786238af5574738d65e5f643794c
Yufan You <ouuansteve@gmail.com>
2021-08-07 11:51:39 +0800

    chore: fix copyright notice

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright [yyyy] [name of copyright owner]
+ * Copyright 2021 Yufan You
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
